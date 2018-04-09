47043a78bb34cdcb375f28c3d600b556b37543e3
Vincent <giardina.v@gmail.com>
2018-04-09 17:04:27 +0200

    maxi clean

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import csv
 names = []
 data = []
 
-with open('data/spambase/formated_spambase.names', 'rt') as f:
-    spamreader = csv.reader(f, delimiter=',', quotechar='~')
-    for row in spamreader:
-        names.append(row[0].strip())
+with open('data/spambase/spambase.formatted_names', 'rt') as f:
+    names = [name.strip() for name in f.readlines()]
 
 with open('data/spambase/spambase.data', 'rt') as f:
-    spamreader = csv.reader(f, delimiter=',', quotechar='~')
-    for row in spamreader:
-        data.append({name:row[k] for (k,name) in enumerate(names)})
+    data_reader = csv.reader(f, delimiter=',', quotechar='~')
+    data = list(map(lambda input: {name : float(input[k]) for (k , name) in enumerate(names)}, data_reader))
 
-def get_perf(name):
-    spam_perf = sum([float(input[name]) for input in data if input["is_spam"] == "1"]) / len(data)
-    regular_perf = sum([float(input[name]) for input in data if input["is_spam"] == "0"]) / len(data)
-    return (spam_perf, regular_perf)
+spam = list(filter(lambda x: x["is_spam"] == 1, data))
+regular = list(filter(lambda x: x["is_spam"] == 0, data))
 
-print(get_perf("word_freq_make"))
+
+def get_perf(n, d):
+    return sum([input[n] for input in d]) / len(d) * 100
+
+def sort_perf(d):
+    return sorted({ n:get_perf(n,d) for n in names }, key=lambda name: get_perf(name, d))
+
+print(get_perf("word_freq_george", data))
+
+print(sort_perf(data))
