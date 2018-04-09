025d9140888e984553143f25677163813dfd36e1
Vincent <giardina.v@gmail.com>
2018-04-09 16:31:44 +0200

    get perf from name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 names = []
 data = []
 
-with open('data/spambase/spambase.names', 'rt') as f:
+with open('data/spambase/formated_spambase.names', 'rt') as f:
     spamreader = csv.reader(f, delimiter=',', quotechar='~')
     for row in spamreader:
         names.append(row[0].strip())
@@ -13,4 +13,9 @@ with open('data/spambase/spambase.data', 'rt') as f:
     for row in spamreader:
         data.append({name:row[k] for (k,name) in enumerate(names)})
 
-print(data)
+def get_perf(name):
+    spam_perf = sum([float(input[name]) for input in data if input["is_spam"] == "1"]) / len(data)
+    regular_perf = sum([float(input[name]) for input in data if input["is_spam"] == "0"]) / len(data)
+    return (spam_perf, regular_perf)
+
+print(get_perf("word_freq_make"))
