b287f17785ec7149c1b3c24141118b56ef9c7ddd
ssssarah <sarah@cityparking.info>
2018-04-09 16:36:55 +0200

    to float

--- src/test_sarah.py
+++ src/test_sarah.py
@@ -12,7 +12,7 @@ name_reader = [x.strip() for x in name_file.readlines()]
 def to_dictionary(line):
     dictionary = {}
     for i in range(0, len(line)):
-        dictionary[name_reader[i]] = line[i]
+        dictionary[name_reader[i]] = float(line[i])
     return dictionary
 
 
@@ -21,7 +21,7 @@ data = list(map(lambda x: to_dictionary(x), data_reader))
 #  for entry in data[:10]:
 #    print(entry)
 
-spam = list(filter(lambda x: x["is_spam"] == '1', data))
+spam = list(filter(lambda x: x["is_spam"] == 1, data))
 not_spam = list(filter(lambda x: x["is_spam"] == 0, data))
 
 print(len(spam)) # 1813
