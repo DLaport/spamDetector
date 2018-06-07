db9d135b641cd958fa4ca1d5b749f8a9c4d97308
Vincent <giardina.v@gmail.com>
2018-06-07 18:47:54 +0200

    try cleaning

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 os.chdir('./spambase/')
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 data = pd.read_csv('spambase.data',header = None)
 
@@ -11,8 +13,35 @@ data.head()
 
 #data.info()
 
-#data.describe()
+data.describe()
 
+# DATA CLEANING
+names = pd.read_csv('spambase.formatted_names', header = None)[0]
+
+spam = data.loc[data[57] == 1]
+spam_feature_means = spam.mean(axis=0)
+
+regular = data.loc[data[57] == 0]
+regular_feature_means = regular.mean(axis=0)
+
+means_ratios = [
+    max(spam_feature_means[i], regular_feature_means[i]) / min(spam_feature_means[i], regular_feature_means[i])
+    for i in range(0,56)]
+
+del names[56]
+del names[57]
+plt.stem(names, means_ratios)
+plt.ylim(0, 3)
+#plt.show()
+
+names_ratios = [(names[i], means_ratios[i]) for i in range(0,56)]
+sorted_ratios = sorted(names_ratios, key=lambda nr: nr[1])
+
+print(sorted_ratios)
+
+for i in range(56):
+    if means_ratios[i] < 20:
+        data.drop(i, 1)
 
 data[data.columns[57]].value_counts(1)
 
@@ -43,4 +72,4 @@ precision_LR = precision_score(y_test,output_LR)
 recall_LR = recall_score(y_test,output_LR)
 f1_score = f1_score(y_test,output_LR)
 auc = roc_auc_score(y_test,output_LR)
-#print('accuracy ',accuracy_LR,'precision ',precision_LR,'recall ', recall_LR, 'f1_score ', f1_score,'auc ',auc)
+print('accuracy ',accuracy_LR,'precision ',precision_LR,'recall ', recall_LR, 'f1_score ', f1_score,'auc ',auc)
